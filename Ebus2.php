dbd89dac85c489bb8e17cd2094f35663d0499d5d
Dave <drossiter7@gmail.com>
2018-02-26 14:31:15 +0000

    update

--- Ebus2.php
+++ Ebus2.php
@@ -12,8 +12,14 @@
           
           <form action ="Ebus.php" method="Post">
              <label for = "user_pin">PIN</label>
-             <input type = "password" id= "user_pin"
+             <input type = "password" id= "user_pin" placeholder="Card PIN" maxlenght="4">
+             
+             <button type="Submit" id="btnPurchase" disabled>Proceed with Purchase</button>
               
           </form>
+          <br/>
+          <button onClick="validateDetails()"Validate></button>
+              
+          <script type="text/javascript" src="Ebus2_validator.js"></script>
       </body>
 </html>
